2c08f4d796fca893efea922d47382330d3a3da00
Rafael B. Tauil <rafael@tauil.com.br>
2012-03-31 09:45:53 -0300

    Adds mobile layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,13 @@ class ApplicationController < ActionController::Base
     @@enki_config = Enki::Config.default
   end
   helper_method :enki_config
+
+  def mobile?
+    request.headers["HTTP_USER_AGENT"].include?(("iPhone" or "BlackBerry" or "SymbianOS" or "Android")) unless (params[:mobile] == "false")
+  end
+  helper_method :mobile?
+
+  def mobile_view_path
+    prepend_view_path "app/views/mobile" if mobile?
+  end
 end
